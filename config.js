812b51c2c36cf39a75ef6c4ccd63eab1443e6cb2
aundre124 <aundre124@gmail.com>
2024-06-04 16:45:45 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const PORT = 3000
+const MongoDBURL = "mongodb+srv://aundre124:darkman11@todocluster.fyolt9i.mongodb.net/?retryWrites=true&w=majority&appName=toDoCluster"
+
+export { MongoDBURL, PORT };
